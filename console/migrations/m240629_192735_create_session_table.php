0122fdf6447384ff7b6e6f482869a38ebc94fb8e
Xekman <oolong12@mail.ru>
2024-06-29 22:28:39 +0300

    Создание yii2 проекта

---
+++ console/migrations/m240629_192735_create_session_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%session}}`.
+ */
+class m240629_192735_create_session_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%session}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%session}}');
+    }
+}
