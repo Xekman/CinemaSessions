6a1af0bdbe7139b1d7339d0b6b12d16c5d8ce280
Xekman <oolong12@mail.ru>
2024-06-30 16:04:31 +0300

    Контроллер для добавления юзера в бд (регистрация) через консоль

---
+++ console/controllers/AddUserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+use common\models\User;
+
+class AddUserController extends Controller
+{
+    public function actionIndex($username, $password, $email)
+    {
+        $user = new User();
+        $user->username = $username;
+        $user->email = $email;
+        $user->setPassword($password);
+        $user->generateAuthKey();
+        if ($user->save()) {
+            echo "User $username has been created.\n";
+        } else {
+            echo "Failed to create user.\n";
+            foreach ($user->errors as $error) {
+                echo $error[0] . "\n";
+            }
+        }
+    }
+}
+
