e1c4ca6487e431aebf7d35810d9a0afc6a60114c
Xekman <oolong12@mail.ru>
2024-06-30 16:05:16 +0300

    Настройки для ролей

--- console/config/main.php
+++ console/config/main.php
@@ -31,6 +31,10 @@ return [
                 ],
             ],
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'defaultRoles' => ['guest'],
+        ],
     ],
     'params' => $params,
 ];
