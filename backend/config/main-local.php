65622c6dd71882e4f50a297f064a529ebe40de25
Xekman <oolong12@mail.ru>
2024-07-01 19:58:04 +0300

    Настройки дял доступа к gii при дев версии

---
+++ backend/config/main-local.php
@@ -0,0 +1,33 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '2K08q7aGiXly6gZy_QmQkrrjfjQWfxsv',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => \yii\debug\Module::class,
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => \yii\gii\Module::class,
+    ];
+}
+if (YII_ENV_DEV) {
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
