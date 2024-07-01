361b57c8d0926b10057bb5507e641a1858ac1158
Xekman <oolong12@mail.ru>
2024-07-01 21:04:19 +0300

    Создание crud системы для фильмов

---
+++ backend/views/film/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use backend\models\Film;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var backend\models\FilmSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Films';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="film-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Film', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'description:ntext',
+            'duration',
+            'age_limit',
+            //'photo_extension',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Film $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
