4bdf2613a7f30aa95bc0eb838bc0e860bcef954a
Xekman <oolong12@mail.ru>
2024-07-02 21:44:16 +0300

    Изменение представления дял фронт части. Отображение всех необходимых функций на экран

--- frontend/views/site/schedule.php
+++ frontend/views/site/schedule.php
@@ -1,11 +1,15 @@
 <?php
 
-use yii\grid\GridView;
 use yii\helpers\Html;
+use yii\grid\GridView;
+use common\models\Session;
+
+/** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->title = 'Расписание сеансов';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-
 <div class="site-schedule">
     <h1><?= Html::encode($this->title) ?></h1>
 
@@ -17,16 +21,41 @@ $this->title = 'Расписание сеансов';
                 'label' => 'Фильм',
             ],
             [
+                'attribute' => 'film.photo_extension',
+                'label' => 'Постер',
+                'format' => 'html',
+                'value' => function ($model) {
+                    if ($model->film->photo_extension) {
+                        return Html::img(Yii::getAlias('@web') . '/upload/film/' . $model->film->id . '.' . $model->film->photo_extension, ['width' => '100']);
+                    } else {
+                        return 'No photo';
+                    }
+                },
+            ],
+            [
                 'attribute' => 'date',
                 'label' => 'Дата',
+                'enableSorting' => false,
             ],
             [
                 'attribute' => 'time',
                 'label' => 'Время',
+                'enableSorting' => false,
+            ],
+            [
+                'attribute' => 'film.duration',
+                'label' => 'Продолжительность',
+                'value' => function ($model) {
+                    return $model->film->duration . ' ч.';
+                },
             ],
             [
                 'attribute' => 'cost',
                 'label' => 'Стоимость',
+                'enableSorting' => false,
+                'value' => function ($model) {
+                    return $model->cost . ' ₽';
+                },
             ],
         ],
     ]); ?>
